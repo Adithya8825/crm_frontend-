145420d3ba89237a18eaee3d38ff9f2c7cacf436
Adithya j <159424093+Adithya8825@users.noreply.github.com>
2025-07-02 20:37:49 +0530

    Update AdminDashboard.jsx

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import AdminSidebar from './AdminSidebar';
-import AdminHeader from './AdminHeader';
-import DashboardHome from './DashboardHome';
-import UserManagement from './UserManagement';
-import TaskManager from './TaskManager';
-import RoleEditor from './RoleEditor';
-
-export default function AdminDashboard() {
-  return (
-    <div className="flex bg-gray-100 min-h-screen">
-      <AdminSidebar />
-      <div className="flex-1">
-        <AdminHeader username="Master Admin" />
-        <main className="p-4 space-y-6">
-          <DashboardHome />
-          <UserManagement />
-          <RoleEditor />
-          <TaskManager />
-        </main>
-      </div>
-    </div>
-  );
-}
 // src/components/admin/AdminDashboard.jsx
 import React from 'react';
 import { Routes, Route, NavLink, useLocation } from 'react-router-dom';
@@ -118,4 +93,3 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
->>>>>>> f8462e0 (Initial commit from crm_frontend--main)
